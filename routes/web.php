6227df51b2e7481c70527f3dacc9dcf008f93635
arari3s <arlanariandi@gmail.com>
2023-02-12 22:57:25 +0700

    UPDATE: Update middleware SetLocale

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,20 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', WelcomeController::class);
-Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('locale')->group(function () {
+    Route::get('/', WelcomeController::class);
+    Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::controller(LanguageController::class)->group(function () {
-    Route::get('language', 'create')->name('language');
-    Route::post('language', 'store');
+    Route::controller(LanguageController::class)->group(function () {
+        Route::get('language', 'create')->name('language');
+        Route::post('language', 'store');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    require __DIR__ . '/auth.php';
 });
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__ . '/auth.php';
